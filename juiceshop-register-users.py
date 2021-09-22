b40d973dd748ca9689a943998b953d0e6a55d363
Ashish Gupta <ashishmgupta@hotmail.com>
2021-09-22 18:32:22 -0400

    Update juiceshop-register-users.py

--- juiceshop-register-users.py
+++ juiceshop-register-users.py
@@ -1,22 +1,22 @@
 # Author : Ashish Gupta
-# Registers specificed number of users in the JuiceShop application
+# Registers specificed number of users in the JuiceShop application [https://owasp.org/www-project-juice-shop/]
 # This can be used as baseline for how users get created in the JuiceShop application.
 # Ideally this pattern should be learnt by a WAF product. So if there is an anomaly in the request, WAF can recognize and alert.
-# 
-# TODO : 
+
+# TODO :
 #   a) Renerate the prefix of the email address instead of asking the user.
 #   b) Remove hardcoding of values in the request payload.
+#   c) Add exception handling
 import requests
 import json
-
-url = "https://lpl-juice-shop-f5-silverline.dev.lpl.com/api/Users"
+url = ""
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/json"
 }
 user_prefix = ""
 
-
+# TODO : Renerate the prefix of the email address instead of asking the user
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
@@ -54,6 +54,9 @@ def create_users(number_of_users):
             #print("User " + str(response_json["email"]) + " created successfully!")
             print(response.text)
 
+url = input("Enter your JuiceShop application URL : ")
+url += "/api/Users"
+print(url)
 user_prefix = input("Enter the prefix of the user email address so It can suffix with an incremental number.\n e.g. If you supply \"user\", users created will be user_1@example.com, user_2@example.com and so on...\n Enter the prefix now : ")
 number_of_users = int(input("Enter the number of users to be created : "))
 create_users(number_of_users)
